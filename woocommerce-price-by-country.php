590293de214bb356a77d282b9487ff142c5cd371
Sweet Homes <Sweet Homes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-14 15:21:56 +0000

    solved naming function of pbc_remove_loop_button
    git-svn-id: https://plugins.svn.wordpress.org/woocomerce-price-by-country/trunk@1007361 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-price-by-country.php
+++ woocommerce-price-by-country.php
@@ -1022,10 +1022,10 @@ function pbc_get_permited_countries() {
 $countries = pbc_get_permited_countries();
 
 if($countries == 'outside'){
-	add_action('init','remove_loop_button');
+	add_action('init','pbc_remove_loop_button');
 }
 
-function remove_loop_button(){
+function pbc_remove_loop_button(){
 	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 	
